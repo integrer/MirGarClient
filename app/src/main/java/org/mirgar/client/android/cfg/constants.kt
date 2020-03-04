0e5b7134b82dbfb5bd5b47d54ff70d8117afd797
Nikita Bibick <osbib2@gmail.com>
2020-03-04 17:53:57 +0300

    Add empty Android app project.

---
+++ app/src/main/java/org/mirgar/client/android/cfg/constants.kt
@@ -0,0 +1,10 @@
+package org.mirgar.client.android.cfg
+
+class Joomla(val host: String, val useSsl: Boolean, val apiPluginName: String) {
+    companion object {
+        private const val API_PLUGIN_NAME: String = "mirgar"
+
+        fun debug() = Joomla("", false, API_PLUGIN_NAME)
+        fun release() = Joomla("mirgar.ga", true, API_PLUGIN_NAME)
+    }
+}
