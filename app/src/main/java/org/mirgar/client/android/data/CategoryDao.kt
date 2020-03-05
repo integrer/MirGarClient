974240726c25649d2fe545ae8245df1a29c8a7c6
Nikita Bibick <osbib2@gmail.com>
2020-03-05 20:36:56 +0300

    Add insert method.

--- app/src/main/java/org/mirgar/client/android/data/CategoryDao.kt
+++ app/src/main/java/org/mirgar/client/android/data/CategoryDao.kt
@@ -1,7 +1,9 @@
 package org.mirgar.client.android.data
 
 import androidx.lifecycle.LiveData
+
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Transaction
 
@@ -12,4 +14,14 @@ interface CategoryDao {
     @Transaction
     @Query("SELECT * FROM categories WHERE id IN (SELECT DISTINCT(super_id) FROM categories)")
     fun getCategoriesWithSubcategories(): LiveData<List<CategoryRelations>>
+
+    @Transaction
+    @Query("SELECT * FROM categories WHERE id = :id")
+    fun getSubcategories(id: Long): LiveData<CategoryRelations>
+
+    @Query("SELECT * FROM categories WHERE super_id IS NULL")
+    fun getRoots(): LiveData<List<Category>>
+
+    @Insert
+    fun insert(category: Category)
 }
