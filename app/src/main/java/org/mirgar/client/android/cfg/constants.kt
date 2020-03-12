88a271b54295b552d1497e7aa6347bfde438b12c
Nikita Bibick <osbib2@gmail.com>
2020-03-12 11:04:46 +0300

    Small fix

--- app/src/main/java/org/mirgar/client/android/cfg/constants.kt
+++ app/src/main/java/org/mirgar/client/android/cfg/constants.kt
@@ -1,11 +1,11 @@
 package org.mirgar.client.android.cfg
 
-class Joomla(val host: String, val useSsl: Boolean, val apiPluginName: String) {
+class Joomla private constructor(val host: String, val useSsl: Boolean, val apiPluginName: String) {
     companion object {
         private const val API_PLUGIN_NAME: String = "mirgar"
 
-        fun debug() = Joomla("", false, API_PLUGIN_NAME)
-        fun release() = Joomla("mirgar.ga", true, API_PLUGIN_NAME)
+        val debug by lazy { Joomla("127.0.0.1:8080", false, API_PLUGIN_NAME) }
+        val release by lazy { Joomla("mirgar.ga", true, API_PLUGIN_NAME) }
     }
 }
 
