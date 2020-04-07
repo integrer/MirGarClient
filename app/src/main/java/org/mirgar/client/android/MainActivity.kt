06645413a2a9a9a9e727e1986737a57f3b759c29
integrer <osbib2@gmail.com>
2020-04-07 18:47:18 +0300

    Setup navigate up action with navigation component

--- app/src/main/java/org/mirgar/client/android/MainActivity.kt
+++ app/src/main/java/org/mirgar/client/android/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,12 +21,19 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.dst_my_appeals, R.id.navigation_dashboard, R.id.navigation_notifications
+        appBarConfiguration = AppBarConfiguration
+            .Builder(
+                R.id.dst_my_appeals,
+                R.id.navigation_dashboard,
+                R.id.navigation_notifications
             )
-        )
+            .build()
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
 }
