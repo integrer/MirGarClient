a9c489878f36b31c5043a95d710369ddd556b588
integrer <osbib2@gmail.com>
2020-04-08 21:25:44 +0300

    Create list item layout

--- app/src/main/java/org/mirgar/client/android/ui/adapters/BindingAdapters.kt
+++ app/src/main/java/org/mirgar/client/android/ui/adapters/BindingAdapters.kt
@@ -1,7 +1,10 @@
 package org.mirgar.client.android.ui.adapters
 
 import android.view.View
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 
 @BindingAdapter("isGone")
 fun bindIsGone(view: View, isGone: Boolean) {
@@ -9,5 +12,15 @@ fun bindIsGone(view: View, isGone: Boolean) {
         View.GONE
     } else {
         View.VISIBLE
+    }
+}
+
+@BindingAdapter("tryLoadImageFrom")
+fun tryLoadImageFrom(view: ImageView, uri: String?) {
+    uri?.let { imageUri ->
+        Glide.with(view)
+            .load(imageUri)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .into(view)
     }
 }
