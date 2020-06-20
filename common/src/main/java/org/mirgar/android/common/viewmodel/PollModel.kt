6f01f5cfb409b83651b5937ce4cf271860b87122
integrer <osbib2@gmail.com>
2020-06-20 19:34:18 +0300

    Delegate all fields (except options) to other Poll model

--- common/src/main/java/org/mirgar/android/common/viewmodel/PollModel.kt
+++ common/src/main/java/org/mirgar/android/common/viewmodel/PollModel.kt
@@ -7,16 +7,17 @@ abstract class PollModel<out ID, out OptID> {
     abstract val canVote: Boolean
     abstract val options: Collection<PollOption<OptID>>
 
-    fun asNormalized() = this as? NormalizedPollModel ?: NormalizedPollModel.from(this)
+    fun asNormalized() = this as? NormalizedPollModel ?: NormalizedPollModel(this)
 }
 
 class NormalizedPollModel<out ID, out OptID> internal constructor(
-    override val id: ID,
-    override val name: CharSequence,
-    override val showResults: Boolean,
-    override val canVote: Boolean,
-    options: Collection<PollOption<OptID>>
+    private val other: PollModel<ID, OptID>
 ) : PollModel<ID, OptID>() {
+    override val id get() = other.id
+    override val name get() = other.name
+    override val showResults get() = other.showResults
+    override val canVote get() = other.canVote
+
     override val options: Collection<NormalizedPollOption<OptID>>
 
     class NormalizedPollOption<out ID> internal constructor(
@@ -27,18 +28,13 @@ class NormalizedPollModel<out ID, out OptID> internal constructor(
         NormalizedPollOption(id, name, votes.toDouble() / total)
 
     init {
-        val optsSeq = options.asSequence()
+        val optsSeq = other.options.asSequence()
         val totalVotes by lazy { optsSeq.map { it.votes.toDouble() }.sum() }
 
         val normalizedSeq = optsSeq.map { it.normalizeBy(totalVotes) }
 
         this.options =
-            ArrayList<NormalizedPollOption<OptID>>(options.size).apply { addAll(normalizedSeq) }
-    }
-
-    companion object {
-        fun <ID, OptID> from(o: PollModel<ID, OptID>) =
-            NormalizedPollModel(o.id, o.name, o.showResults, o.canVote, o.options)
+            ArrayList<NormalizedPollOption<OptID>>(other.options.size).apply { addAll(normalizedSeq) }
     }
 }
 
