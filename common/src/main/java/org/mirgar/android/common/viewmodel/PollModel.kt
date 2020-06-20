ba99046fd29917c42ff0d4be31ac519fa8f4d0ba
integrer <osbib2@gmail.com>
2020-06-20 19:06:22 +0300

    Add id field to poll model

--- common/src/main/java/org/mirgar/android/common/viewmodel/PollModel.kt
+++ common/src/main/java/org/mirgar/android/common/viewmodel/PollModel.kt
@@ -1,27 +1,29 @@
 package org.mirgar.android.common.viewmodel
 
-abstract class PollModel<out ID> {
+abstract class PollModel<out ID, out OptID> {
+    abstract val id: ID
     abstract val name: CharSequence
     abstract val showResults: Boolean
     abstract val canVote: Boolean
-    abstract val options: Collection<PollOption<ID>>
+    abstract val options: Collection<PollOption<OptID>>
 
     fun asNormalized() = this as? NormalizedPollModel ?: NormalizedPollModel.from(this)
 }
 
-class NormalizedPollModel<out ID> internal constructor(
+class NormalizedPollModel<out ID, out OptID> internal constructor(
+    override val id: ID,
     override val name: CharSequence,
     override val showResults: Boolean,
     override val canVote: Boolean,
-    options: Collection<PollOption<ID>>
-) : PollModel<ID>() {
-    override val options: Collection<NormalizedPollOption<ID>>
+    options: Collection<PollOption<OptID>>
+) : PollModel<ID, OptID>() {
+    override val options: Collection<NormalizedPollOption<OptID>>
 
     class NormalizedPollOption<out ID> internal constructor(
         override val id: ID, override val name: CharSequence, override val votes: Double
     ) : PollOption<ID>()
 
-    private fun PollOption<ID>.normalizeBy(total: Double) =
+    private fun PollOption<OptID>.normalizeBy(total: Double) =
         NormalizedPollOption(id, name, votes.toDouble() / total)
 
     init {
@@ -31,12 +33,12 @@ class NormalizedPollModel<out ID> internal constructor(
         val normalizedSeq = optsSeq.map { it.normalizeBy(totalVotes) }
 
         this.options =
-            ArrayList<NormalizedPollOption<ID>>(options.size).apply { addAll(normalizedSeq) }
+            ArrayList<NormalizedPollOption<OptID>>(options.size).apply { addAll(normalizedSeq) }
     }
 
     companion object {
-        fun <ID> from(other: PollModel<ID>) =
-            NormalizedPollModel(other.name, other.showResults, other.canVote, other.options)
+        fun <ID, OptID> from(o: PollModel<ID, OptID>) =
+            NormalizedPollModel(o.id, o.name, o.showResults, o.canVote, o.options)
     }
 }
 
