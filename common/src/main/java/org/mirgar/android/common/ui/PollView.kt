d5ae96dd9a1cc1e17e606e5d53f7116e302d5e37
integrer <osbib2@gmail.com>
2020-06-20 15:51:12 +0300

    Add Poll view class

---
+++ common/src/main/java/org/mirgar/android/common/ui/PollView.kt
@@ -0,0 +1,17 @@
+package org.mirgar.android.common.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class PollView : View {
+    constructor(context: Context) : super(context)
+
+    @Suppress("UNUSED")
+    constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int = 0,
+        defStyleRes: Int = 0
+    ) : super(context, attrs, defStyleAttr, defStyleRes)
+}
