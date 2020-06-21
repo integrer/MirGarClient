9632fa8298e8ac97702c9d0679c01902977051c0
integrer <osbib2@gmail.com>
2020-06-21 20:01:08 +0300

    Add kodein view model factory with args

--- cryptoelection/src/main/java/org/mirgar/android/cryptoelection/viewmodel/KodeinVMFactory.kt
+++ cryptoelection/src/main/java/org/mirgar/android/cryptoelection/viewmodel/KodeinVMFactory.kt
@@ -5,10 +5,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import org.kodein.di.DKodein
-import org.kodein.di.Kodein
-import org.kodein.di.KodeinAware
-import org.kodein.di.direct
+import org.kodein.di.*
 import org.kodein.di.generic.bind
 import org.kodein.di.generic.instanceOrNull
 
@@ -21,11 +18,27 @@ class KodeinVMFactory(private val injector: DKodein) : ViewModelProvider.Factory
         injector.instanceOrNull<ViewModel>(tag = modelClass.ident()) as T? ?: modelClass.newInstance()
 }
 
+class KodeinVMFactoryWithArgs<A : Any>(
+    private val injector: DKodein, private val args: Typed<A>
+) : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T =
+        injector.instanceOrNull<A, ViewModel>(modelClass.ident(), args) as T?
+            ?: modelClass.newInstance()
+
+    companion object {
+        inline fun <reified A : Any> new(injector: DKodein, args: A): KodeinVMFactoryWithArgs<A> {
+            return KodeinVMFactoryWithArgs(injector, Typed(generic(), args))
+        }
+    }
+}
+
 @MainThread
 inline fun <reified VM : ViewModel, T> T.viewModels(): Lazy<VM> where T : Fragment, T : KodeinAware =
     viewModels(factoryProducer = ::kodeinVMFactory)
 
 fun <T: KodeinAware> T.kodeinVMFactory() = KodeinVMFactory(direct)
+inline fun <T : KodeinAware, reified A : Any> T.kodeinVMFactory(args: A) = KodeinVMFactoryWithArgs.new(direct, args)
 
 fun <T> Class<T>.ident() = name
 
