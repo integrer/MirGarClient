7509b7d7b80a094165a714c2d75511b88547d24c
integrer <osbib2@gmail.com>
2020-06-21 10:07:32 +0300

    Make Poll model view model.

--- common/src/main/java/org/mirgar/android/common/viewmodel/PollViewModel.kt
+++ common/src/main/java/org/mirgar/android/common/viewmodel/PollViewModel.kt
@@ -1,18 +1,20 @@
 package org.mirgar.android.common.viewmodel
 
-abstract class PollModel<out ID, out OptID> {
+import androidx.lifecycle.ViewModel
+
+abstract class PollViewModel<out ID, out OptID> : ViewModel() {
     abstract val id: ID
     abstract val name: CharSequence
     abstract val showResults: Boolean
     abstract val canVote: Boolean
     abstract val options: Collection<PollOption<OptID>>
 
-    fun asNormalized() = this as? NormalizedPollModel ?: NormalizedPollModel(this)
+    fun asNormalized() = this as? NormalizedPollViewModel ?: NormalizedPollViewModel(this)
 }
 
-class NormalizedPollModel<out ID, out OptID> internal constructor(
-    private val other: PollModel<ID, OptID>
-) : PollModel<ID, OptID>() {
+class NormalizedPollViewModel<out ID, out OptID> internal constructor(
+    private val other: PollViewModel<ID, OptID>
+) : PollViewModel<ID, OptID>() {
     override val id get() = other.id
     override val name get() = other.name
     override val showResults get() = other.showResults
