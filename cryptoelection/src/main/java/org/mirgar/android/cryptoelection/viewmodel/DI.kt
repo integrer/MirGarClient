6154ee39c6c0db5c36453c0a91c8092afb28c3a6
integrer <osbib2@gmail.com>
2020-06-21 23:22:48 +0300

    Bind view models with its types instead using tag

--- cryptoelection/src/main/java/org/mirgar/android/cryptoelection/viewmodel/DI.kt
+++ cryptoelection/src/main/java/org/mirgar/android/cryptoelection/viewmodel/DI.kt
@@ -1,21 +1,22 @@
 package org.mirgar.android.cryptoelection.viewmodel
 
 import org.kodein.di.Kodein
+import org.kodein.di.generic.bind
 import org.kodein.di.generic.instance
 import org.kodein.di.generic.provider
 
 fun Kodein.Builder.bindViewModels() {
-    bindViewModel<AuthorizationViewModel>() with provider {
+    bind<AuthorizationViewModel>() with provider {
         AuthorizationViewModelImpl(
             instance(),
             instance()
         )
     }
-    bindViewModel<RegistrationViewModel>() with provider {
+    bind<RegistrationViewModel>() with provider {
         RegistrationViewModel(
             instance(),
             instance()
         )
     }
-    bindViewModel<ElectionsListViewModel>() with provider { ElectionsListViewModel() }
+    bind<ElectionsListViewModel>() with provider { ElectionsListViewModel() }
 }
--- cryptoelection/src/main/java/org/mirgar/android/cryptoelection/viewmodel/KodeinVMFactory.kt
+++ cryptoelection/src/main/java/org/mirgar/android/cryptoelection/viewmodel/KodeinVMFactory.kt
@@ -6,30 +6,24 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import org.kodein.di.*
-import org.kodein.di.generic.bind
-import org.kodein.di.generic.instanceOrNull
 
 /**
  * View model factory for easy injecting view models with their dependencies using Kodein library
  */
 class KodeinVMFactory(private val injector: DKodein) : ViewModelProvider.Factory {
-    @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T =
-        injector.instanceOrNull<ViewModel>(tag = modelClass.ident()) as T?
-            ?: modelClass.newInstance()
+        injector.InstanceOrNull(TT(modelClass)) ?: modelClass.newInstance()
 }
 
 class KodeinVMFactoryWithArgs<A : Any>(
-    private val injector: DKodein, private val args: Typed<A>
+    private val injector: DKodein, private val args: A, private val argType: TypeToken<in A>
 ) : ViewModelProvider.Factory {
-    @Suppress("UNCHECKED_CAST")
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T =
-        injector.instanceOrNull<A, ViewModel>(modelClass.ident(), args) as T?
-            ?: modelClass.newInstance()
+    override fun <T : ViewModel> create(modelClass: Class<T>): T =
+        injector.InstanceOrNull(argType, TT(modelClass), arg = args) ?: modelClass.newInstance()
 
     companion object {
         inline fun <reified A : Any> new(injector: DKodein, args: A): KodeinVMFactoryWithArgs<A> {
-            return KodeinVMFactoryWithArgs(injector, Typed(generic(), args))
+            return KodeinVMFactoryWithArgs(injector, args, generic())
         }
     }
 }
@@ -41,8 +35,3 @@ inline fun <reified VM : ViewModel, T> T.viewModels(): Lazy<VM> where T : Fragme
 fun <T : KodeinAware> T.kodeinVMFactory() = KodeinVMFactory(direct)
 inline fun <T : KodeinAware, reified A : Any> T.kodeinVMFactory(args: A) =
     KodeinVMFactoryWithArgs.new(direct, args)
-
-fun <T> Class<T>.ident() = name
-
-inline fun <reified VM : ViewModel> Kodein.Builder.bindViewModel(overrides: Boolean? = null) =
-    bind<ViewModel>(VM::class.java.ident(), overrides)
