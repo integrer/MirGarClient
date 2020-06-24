f3d4c2f60c4e0d74b37a03dfb4199b813957bf60
integrer <osbib2@gmail.com>
2020-06-24 16:20:28 +0300

    Add iterable converters

--- common/src/main/java/org/mirgar/android/common/data/Converters.kt
+++ common/src/main/java/org/mirgar/android/common/data/Converters.kt
@@ -12,3 +12,13 @@ class Converters {
         timeInMillis = timestamp
     }
 }
+
+@Suppress("UNCHECKED_CAST")
+fun <T> Iterable<T>.tryMatchAsList(): List<T>? = when (this) {
+    is List<T> -> this
+    is Array<*> -> this.asList() as List<T>
+    else -> null
+}
+
+fun <T> Iterable<T>.tryMatchAsListOrConvert(): List<T> = tryMatchAsList() ?: toList()
+
