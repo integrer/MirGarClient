4a217afbda8f32ef801376cc686b0bcf7d37c1af
integrer <osbib2@gmail.com>
2020-06-26 16:48:50 +0300

    Add lazyMap function

--- common/src/main/java/org/mirgar/android/common/data/Converters.kt
+++ common/src/main/java/org/mirgar/android/common/data/Converters.kt
@@ -1,5 +1,8 @@
 package org.mirgar.android.common.data
 
+import android.util.SparseArray
+import androidx.core.util.getOrElse
+import androidx.core.util.set
 import androidx.room.TypeConverter
 import java.util.*
 
@@ -21,4 +24,22 @@ fun <T> Iterable<T>.tryMatchAsList(): List<T>? = when (this) {
 }
 
 fun <T> Iterable<T>.tryMatchAsListOrConvert(): List<T> = tryMatchAsList() ?: toList()
+
+/**
+ * Lazily initializes new list using sparse array under the hood
+ */
+fun <T, R> List<T>.lazyMap(transform: (T) -> R): List<R> = LazyMap(this, transform)
+
+private class LazyMap<T, R>(
+    private val input: List<T>,
+    private val transform: (T) -> R
+) : AbstractList<R>() {
+    override val size = input.size
+
+    val inner by lazy { SparseArray<R>(size) }
+
+    override fun get(index: Int): R = inner.getOrElse(index) {
+        transform(input[index]).also { inner[index] = it }
+    }
+}
 
