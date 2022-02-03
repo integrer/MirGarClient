e79d10befff8b6effc1cb071675fff4f5a496221
integrer <osbib2@gmail.com>
2022-02-03 22:00:44 +0300

    Add nulable values support

--- mgclient/src/main/java/org/mirgar/android/mgclient/data/Converters.kt
+++ mgclient/src/main/java/org/mirgar/android/mgclient/data/Converters.kt
@@ -3,6 +3,8 @@ package org.mirgar.android.mgclient.data
 import androidx.room.TypeConverter
 
 class Converters {
-    @TypeConverter fun intoAppealStatus(id: Int) = AppealStatus.from(id)
-    @TypeConverter fun fromAppealStatus(from: AppealStatus) = from.id
+    @TypeConverter
+    fun intoAppealStatus(id: Int?) = id?.let { AppealStatus.from(it) }
+    @TypeConverter
+    fun fromAppealStatus(from: AppealStatus?) = from?.id
 }
