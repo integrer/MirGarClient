b71287f9daafb4055a5732016239f6d2498ea55e
integrer <osbib2@gmail.com>
2022-02-03 22:00:06 +0300

    set joomla ref to mirgar.ga constantly

--- mgclient/src/main/java/org/mirgar/android/mgclient/cfg/constants.kt
+++ mgclient/src/main/java/org/mirgar/android/mgclient/cfg/constants.kt
@@ -1,14 +1,12 @@
 package org.mirgar.android.mgclient.cfg
 
-import org.mirgar.android.mgclient.BuildConfig
-
 class Joomla private constructor(host: String, useSsl: Boolean) {
     val hostUrl = "${if (useSsl) "https" else "http"}://$host"
 
     companion object {
         private val debug by lazy { Joomla("10.0.2.2:8088", false) }
         private val release by lazy { Joomla("mirgar.ga", true) }
-        val current get() = if (BuildConfig.DEBUG) debug else release
+        val current get() = /* if (BuildConfig.DEBUG) debug else */ release
     }
 }
 
